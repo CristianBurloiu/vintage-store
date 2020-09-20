2efdf6b3b6d769aa4e7de385df67eae5a851a258
oscarmdavies <cristianburloiu@gmail.com>
2020-09-20 19:03:32 +0100

    Updates

---
+++ precache-manifest.58962f7debafe64e80d41d9e7e2be7b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ec826752f1d2238a7866a427db13de",
+    "url": "/vintage-store.git/index.html"
+  },
+  {
+    "revision": "fe2bb7f9e3abf1126297",
+    "url": "/vintage-store.git/static/css/main.0528a603.chunk.css"
+  },
+  {
+    "revision": "cbf8ed4748a9e59b5112",
+    "url": "/vintage-store.git/static/js/2.f61e2e97.chunk.js"
+  },
+  {
+    "revision": "fe2bb7f9e3abf1126297",
+    "url": "/vintage-store.git/static/js/main.6760d683.chunk.js"
+  },
+  {
+    "revision": "114bc0b17b4ac852cca6",
+    "url": "/vintage-store.git/static/js/runtime-main.25aee57c.js"
+  },
+  {
+    "revision": "8894b7d37bd41e50b31318e97d50b323",
+    "url": "/vintage-store.git/static/media/loading.8894b7d3.gif"
+  },
+  {
+    "revision": "7ac3ba3ab8503352edfc941d7ecd430d",
+    "url": "/vintage-store.git/static/media/logo.7ac3ba3a.svg"
+  },
+  {
+    "revision": "016d4fcc158e863ea29afc6b6ff6c3ff",
+    "url": "/vintage-store.git/static/media/mainBcg.016d4fcc.jpeg"
+  }
+]);
